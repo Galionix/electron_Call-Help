6c52838c3825232561094b9943d318290cf796a0
galionix <parfumeland@ya.ru>
2020-12-19 07:50:57 +0200

    new model improvements

--- chatpage/scripts/action.parts.js
+++ chatpage/scripts/action.parts.js
@@ -18,11 +18,58 @@ function signOut() {
 }
 
 // SPLITVIEW
+function applySplit() {
+    
+    $("#message_wrapper").hide();
+    $("#userlist_wrapper").hide();
+    $("#chat_menu_wrapper").hide();
+if(view_chat_menu_wrapper == "true") 
+   { $("#chat_menu_wrapper").show(); return }
+    if(view_message_wrapper == "true") 
+    {$("#message_wrapper").show();  return }
+    if(view_userlist_wrapper == "true") 
+    $("#userlist_wrapper").show();
+    
 
+}
+
+function windowResize() {
+  // Get width and height of the window excluding scrollbars
+  var w = document.documentElement.clientWidth;
+
+  // Display result inside a div element
+  if (w < 400) {
+    splitview = "true";
+    applySplit();
+  } else {
+    splitview = "false";
+
+    $("#message_wrapper").hide();
+    $("#userlist_wrapper").hide();
+    $("#chat_menu_wrapper").hide();
+    
+    if(view_message_wrapper == "true") 
+    $("#message_wrapper").show();
+    if(view_userlist_wrapper == "true") 
+    $("#userlist_wrapper").show();
+    if(view_chat_menu_wrapper == "true") 
+    $("#chat_menu_wrapper").show();
+  }
+  localStorage.setItem("splitview", splitview);
+
+
+// $('#cmw_info').css('width',  $('#chat_menu_wrapper').css('width'));
+//   document.getElementById("result").innerHTML =
+//     "Width: " + w + "splitview " + splitview;
+}
+window.addEventListener("resize", windowResize);
+windowResize();
 // FULLWIEV
 
 // MENU
-
+$("#cmw_search_input").on('click',function () {
+  
+})
 // MESSAGES
 
 // USERS
--- chatpage/scripts/init.parts.js
+++ chatpage/scripts/init.parts.js
@@ -12,6 +12,13 @@
 // console.log("%c 🇬🇫: $('#login_form') ", "font-size:16px;background-color:#4ecad7;color:white;", $('#login_form'))
 
 $('#logged').hide()
+let splitview = localStorage.getItem("splitview") || "false";
+let view_message_wrapper = localStorage.getItem("view_message_wrapper") || "true";
+let view_userlist_wrapper = localStorage.getItem("view_userlist_wrapper") || "false";
+let view_chat_menu_wrapper = localStorage.getItem("view_chat_menu_wrapper") || "true";
+
+// $('#message_wrapper').hide()
+// $('#userlist_wrapper').hide()
 
 
 function onSignIn(googleUser) {
@@ -21,13 +28,26 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    
+    var outside
 
+fetch('https://lh3.googleusercontent.com/a-/AOh14GhWGDbLlTz2rDtlHFbfofvYosm0TknrOrOpLeOw=s192-c')
+  //                         vvvv
+  .then(response => response.blob())
+  .then(images => {
+      // Then create a local URL for that image and print it 
+      outside = URL.createObjectURL(images)
+      console.log(outside)
+  })
 
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-        $('#login_form').hide()
-        $('#logged').show()
+    //https://lh3.googleusercontent.com/a-/AOh14GhWGDbLlTz2rDtlHFbfofvYosm0TknrOrOpLeOw=s192-c
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    $('#login_form').hide()
+    $('#logged').show()
+    $('#user_avatar').attr("src",profile.getImageUrl());
+    $('#username').text(profile.getName())
 }
 
  
